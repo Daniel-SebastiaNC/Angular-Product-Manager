fccccc6f22318e41fee01fab591c66db1de92080
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-06-05 11:10:05 -0300

    feat: add cardComponent to product and feature to list products

---
+++ src/app/features/list/list.component.ts
@@ -0,0 +1,27 @@
+import { Component, inject } from '@angular/core';
+import { ProductsService } from '../../shared/services/products.service';
+import { Product } from '../../shared/interfaces/product.interface';
+import { CardComponent } from './components/card/card.component';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [CardComponent, RouterLink, MatButtonModule],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss'
+})
+export class ListComponent {
+
+  products: Product[] = [];
+
+  productService = inject(ProductsService);
+
+  ngOnInit(){
+    this.productService.getAll().subscribe((data) => {
+      this.products = data;
+    });
+  }
+
+}
